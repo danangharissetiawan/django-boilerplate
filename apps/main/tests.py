40c11cb2bb3af3782745f1dbc3fa97d2731ad23b
danangharissetiawan <60424789+danangharissetiawan@users.noreply.github.com>
2021-07-03 01:21:34 +0700

    add functional test and unittest

--- apps/main/tests.py
+++ apps/main/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestHomePage(TestCase):
+
+  def test_uses_index_template(self):
+    response = self.client.get(reverse("home"))
+    self.assertTemplateUsed(response, "main/home.html")
+  
+  def test_uses_base_template(self):
+    response = self.client.get(reverse("home"))
+    self.assertTemplateUsed(response, "base.html")
+
